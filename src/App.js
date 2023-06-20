eac3d39b56d41494208a20afef9e1c81a08bdb87
Anup Dhondi <60269691+anupdhondi@users.noreply.github.com>
2023-06-20 13:47:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes, Link } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
